801b9162f74e98f1b9c6b7d6faacf832a6e71b4e
Dmitrii Vasilev <dimavas026@yandex.ru>
2022-07-07 02:08:59 +0300

    fix

--- index.js
+++ index.js
@@ -9,38 +9,23 @@ var https = require("https")
 var fs = require("fs")
 require("dotenv").config()
 
-// mongoClient.connect(function(err, client){
-//   if(err) return console.log(err);
-//   app.listen(8000, function(){
-//       console.log("Сервер ожидает подключения...");
-//   });
-// });
-
-// app.get("/api/users", function(req, res){
-
-//   return res.send([1,2,3])
-//   // const collection = req.app.locals.collection;
-//   // collection.find({}).toArray(function(err, users){
-
-//   //     if(err) return console.log(err);
-//   //     res.send(users)
-//   // });
-
-// });
-// to use req.body
-// app.use(express.cookieParser());
-// app.use(cors({ credentials: true, origin: process.env.AUTH_CLIENT_URL }))
 app.use(cookieParser())
-app.set("trust proxy", 1)
-// app.use(cors({ credentials: true }))
+
+// const corsConfig = {
+//   origin: true,
+//   credentials: true,
+// }
+
+// app.use(cors(corsConfig))
+// app.options("*", cors(corsConfig))
+
 app.use(
   cors({
     credentials: true,
     origin: process.env.AUTH_CLIENT_URL,
-    exposedHeaders: ["set-cookie"],
+    // exposedHeaders: ["set-cookie"],
   })
 )
-// app.use(cors({ credentials: true, origin: "*" }))
 app.use(express.json())
 app.use("/api", router)
 
